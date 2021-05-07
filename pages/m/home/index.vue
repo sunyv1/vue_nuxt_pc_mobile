6c43702b073c04478ab5b793657ade99fe8012cc
Zyc <zhangyunchao@ctrlbox.cn>
2021-05-07 15:04:13 +0800

    初始化项目

--- pages/m/home/index.vue
+++ pages/m/home/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <h1>移动主页</h1>
+  <div>
+    <h1>移动主页</h1>
+    <hello />
+    <p-hello />
+  </div>
 </template>
 
 <script>
+import hello from "@/components/m/hello.vue";
+import pHello from "@/components/public/hello.vue";
 export default {
-
-}
+  components: {
+    hello,
+    pHello,
+  },
+};
 </script>
 
 <style>
-
 </style>
--- pages/pc/home/index.vue
+++ pages/pc/home/index.vue
@@ -1,10 +1,18 @@
 <template>
+<div>
   <h1>pc主页</h1>
+  <hello />
+  <p-hello />
+</div>
 </template>
 
 <script>
+import hello from '@/components/pc/hello.vue'
+import pHello from '@/components/public/hello.vue'
 export default {
-
+  components: {
+    hello, pHello
+  }
 }
 </script>
 
